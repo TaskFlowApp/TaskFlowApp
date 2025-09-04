2d83ddf60b24c4772ce1ef76ceee66f3ed010d09
zerone <bazz0324@naver.com>
2025-09-05 03:31:24 +0900

    [comment] feat: comment 엔티티 구현

--- src/main/java/com/taskflowapp/common/entity/BaseEntity.java
+++ src/main/java/com/taskflowapp/common/entity/BaseEntity.java
@@ -1,10 +1,9 @@
 package com.taskflowapp.common.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.*;
 
 import java.time.LocalDateTime;
+
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
@@ -17,8 +16,10 @@ public abstract class BaseEntity {
 
     @CreatedDate
     @Column(updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
     private LocalDateTime createdAt;
 
     @LastModifiedDate
+    @Temporal(TemporalType.TIMESTAMP)
     private LocalDateTime updatedAt;
 }
--- src/main/java/com/taskflowapp/domain/comment/entity/Comment.java
+++ src/main/java/com/taskflowapp/domain/comment/entity/Comment.java
@@ -1,4 +1,41 @@
 package com.taskflowapp.domain.comment.entity;
 
-public class Comment {
+import com.taskflowapp.common.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "comments")
+public class Comment extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // 연관관계 설정 후 변경 예정
+    @Column(nullable = false)
+    private Long userId;
+
+    // 연관관계 설정 후 변경 예정
+    @Column(nullable = false)
+    private Long taskId;
+
+    // 연관관계 설정 후 변경 예정
+    @Column(nullable = false)
+    private Long parentId;
+
+    @Column(nullable = false, length = 100)
+    private String content;
+
+    public Comment(Long id, Long userId, Long taskId, Long parentId, String content) {
+        this.id = id;
+        this.userId = userId;
+        this.taskId = taskId;
+        this.parentId = parentId;
+        this.content = content;
+    }
 }
