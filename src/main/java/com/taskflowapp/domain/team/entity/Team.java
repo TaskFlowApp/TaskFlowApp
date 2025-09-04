d6c9c39473d474137feaf1e030f70b853c7b0910
jinhee kim <lsa68803@naver.com>
2025-09-05 00:28:54 +0900

    [team] feat: team 엔티티, 레포지토리 구현

--- src/main/java/com/taskflowapp/domain/team/entity/Team.java
+++ src/main/java/com/taskflowapp/domain/team/entity/Team.java
@@ -1,4 +1,34 @@
 package com.taskflowapp.domain.team.entity;
 
-public class Team {
+import com.taskflowapp.common.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "teams")
+public class Team extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String teamName;
+
+    @Column(nullable = false, length = 255)
+    private String teamDescription;
+
+    public Team (
+            String teamName,
+            String teamDescription
+    ){
+        this.teamName = teamName;
+        this.teamDescription = teamDescription;
+    }
+
+
 }
