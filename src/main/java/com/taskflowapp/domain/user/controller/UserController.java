9692b76ac589639155c42f8d15bb420687ecf465
cjn404 <moi404@naver.com>
2025-09-05 21:49:54 +0900

    [auth] feat: UserController 구현

---
+++ src/main/java/com/taskflowapp/domain/user/controller/UserController.java
@@ -0,0 +1,40 @@
+package com.taskflowapp.domain.user.controller;
+
+import com.taskflowapp.common.ApiResponse;
+import com.taskflowapp.domain.security.UserDetailsImpl;
+import com.taskflowapp.domain.user.dto.response.UserResponse;
+import com.taskflowapp.domain.user.repository.UserRepository;
+import com.taskflowapp.domain.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("users/")
+public class UserController {
+
+    private final UserRepository userRepository;
+    private final UserService userService;
+
+    // 현재 사용자 정보 조회
+    @GetMapping("/me")
+    public ResponseEntity<ApiResponse<UserResponse>> findUserInfo(
+            @AuthenticationPrincipal UserDetailsImpl userDetails
+    ) {
+        if (userDetails == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body(ApiResponse.error("인증이 필요합니다"));
+        }
+
+        // userDetails에서 username 꺼내서 서비스 호출
+        UserResponse userResponse = userService.findUserInfo(userDetails.getUsername());
+        return ResponseEntity.ok(
+                ApiResponse.success("사용자 정보를 조회했습니다.", userResponse)
+        );
+    }
+}
