c8cc5118f96674071eff7a31403e5578928953ee
byeongsu <113989194+Byeongsu-cmd@users.noreply.github.com>
2025-09-05 09:05:03 +0900

    Update BaseEntity.java

--- src/main/java/com/taskflowapp/common/entity/BaseEntity.java
+++ src/main/java/com/taskflowapp/common/entity/BaseEntity.java
@@ -16,10 +16,8 @@ public abstract class BaseEntity {
 
     @CreatedDate
     @Column(updatable = false)
-    @Temporal(TemporalType.TIMESTAMP)
     private LocalDateTime createdAt;
 
     @LastModifiedDate
-    @Temporal(TemporalType.TIMESTAMP)
     private LocalDateTime updatedAt;
 }
