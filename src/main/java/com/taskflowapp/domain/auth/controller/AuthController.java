d5fd3fbeccd71d5775ae84ce5677a0d70018b729
cjn404 <moi404@naver.com>
2025-09-06 23:15:16 +0900

    [auth] feat: request 입력값 조건(NotBlank, Siz, Pattern 등) 추가

--- src/main/java/com/taskflowapp/domain/auth/controller/AuthController.java
+++ src/main/java/com/taskflowapp/domain/auth/controller/AuthController.java
@@ -6,6 +6,7 @@ import com.taskflowapp.domain.auth.dto.request.AuthRegisterRequest;
 import com.taskflowapp.domain.auth.dto.response.AuthLoginResponse;
 import com.taskflowapp.domain.auth.dto.response.AuthRegisterResponse;
 import com.taskflowapp.domain.auth.service.AuthService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,7 +24,7 @@ public class AuthController {
     // 회원가입
     @PostMapping("/register")
     public ResponseEntity<ApiResponse<AuthRegisterResponse>> register(
-            @RequestBody AuthRegisterRequest request
+            @Valid @RequestBody AuthRegisterRequest request
     ) {
         return ResponseEntity.ok(
                 ApiResponse.success("회원가입이 완료되었습니다.", authService.register(request))
@@ -33,7 +34,7 @@ public class AuthController {
     // 로그인
     @PostMapping("/login")
     public ResponseEntity<ApiResponse<AuthLoginResponse>> login(
-            @RequestBody AuthLoginRequest request
+            @Valid @RequestBody AuthLoginRequest request
     ) {
         return ResponseEntity.ok(
                 ApiResponse.success("로그인이 완료되었습니다.", authService.login(request))
