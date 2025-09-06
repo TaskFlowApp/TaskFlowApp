c7a5076122873aa42c56b0504699aabd64cf3c85
byeongsu-cmd <byeongsukim0607@gmail.com>
2025-09-06 13:53:24 +0900

    feat/members - Member 기능 구현

--- src/main/java/com/taskflowapp/domain/team/dto/TeamResponse.java
+++ src/main/java/com/taskflowapp/domain/team/dto/TeamResponse.java
@@ -1,19 +1,21 @@
 package com.taskflowapp.domain.team.dto;
 
+import com.taskflowapp.domain.user.dto.response.MemberResponseDto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 @AllArgsConstructor
 @Builder
 public class TeamResponse {
 
-    private Long id;
-    private String name;
-    private String description;
-    private LocalDateTime createdAt;
-    // private List<UserResponse> members; 멤버 정보 추후 연결 예정
+    private final Long id;
+    private final String name;
+    private final String description;
+    private final LocalDateTime createdAt;
+    private final List<MemberResponseDto> members;
 }
---
+++ src/main/java/com/taskflowapp/domain/user/controller/MemberController.java
@@ -0,0 +1,53 @@
+package com.taskflowapp.domain.user.controller;
+
+import com.taskflowapp.common.ApiResponse;
+import com.taskflowapp.domain.team.dto.TeamResponse;
+import com.taskflowapp.domain.user.dto.request.MemberRequestDto;
+import com.taskflowapp.domain.user.dto.response.MemberResponseDto;
+import com.taskflowapp.domain.user.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/teams/{teamId}")
+public class MemberController {
+    private final MemberService memberService;
+
+    @PostMapping("/members")
+    public ResponseEntity<ApiResponse<TeamResponse>> addMember(
+//            @AuthenticationPrincipal UserDetailsImpl userDetails,
+            @PathVariable Long teamId,
+            @RequestBody MemberRequestDto memberRequestDto
+    ) {
+//        if (userDetails == null) {
+//            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+//                    .body(ApiResponse.error("인증이 필요합니다"));
+//        }
+        TeamResponse teamResponse = memberService.addMember(teamId, memberRequestDto);
+
+        return ResponseEntity.ok(ApiResponse.success("팀 멤버 추가 성공", teamResponse));
+    }
+
+    @GetMapping("/members") // task가 먼저 만들어져야만 이것을 테스트할 수 있다.. 그래서 아직 테스트하지 못함.
+    public ResponseEntity<ApiResponse<List<MemberResponseDto>>> getMembers(
+            @PathVariable Long teamId
+    ) {
+        List<MemberResponseDto> memberResponse = memberService.findTeamMember(teamId);
+
+        return ResponseEntity.ok(ApiResponse.success("팀 멤버 목록을 조회했습니다.", memberResponse));
+    }
+
+    // 팀 멤버 제거
+    @DeleteMapping("/members/{userId}")
+    public ResponseEntity<ApiResponse<TeamResponse>> deleteMember(
+            @PathVariable Long teamId,
+            @PathVariable Long userId
+    ) {
+        TeamResponse teamResponse = memberService.deleteMember(teamId, userId);
+        return ResponseEntity.ok(ApiResponse.success("멤버가 성공적으로 제거되었습니다.", teamResponse));
+    }
+}
