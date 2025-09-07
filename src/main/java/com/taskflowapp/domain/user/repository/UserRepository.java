4b009d96e3e045973098db2cfcae4ea521837d30
byeongsu-cmd <byeongsukim0607@gmail.com>
2025-09-07 14:47:06 +0900

    feat/members - members 팀 멤버 목록 조회 및 팀 멤버 관리: 추가/삭제 (중복 방지, 존재 여부 검증)

--- src/main/java/com/taskflowapp/domain/user/repository/UserRepository.java
+++ src/main/java/com/taskflowapp/domain/user/repository/UserRepository.java
@@ -3,6 +3,7 @@ package com.taskflowapp.domain.user.repository;
 import com.taskflowapp.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -11,4 +12,10 @@ public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByUsername(String username);
 
     boolean existsByEmail(String email);
+
+    // 팀에 속해 있지 않은 유저만 조회
+    List<User> findAllByTeamIsNull();
+
+    // 팀 아이디를 기준으로 등록되어 있는 유저를 찾아서 출력
+    List<User> findAllByTeamId(Long teamId);
 }
