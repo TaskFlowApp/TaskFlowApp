a35f1a5e89feafd508a9ca8a175842cd068a2abf
cjn404 <moi404@naver.com>
2025-09-07 16:16:43 +0900

    [auth] feat: 이메일 정규식에서 보다 엄격한 검증 조건 추가

--- src/main/java/com/taskflowapp/domain/auth/dto/request/AuthRegisterRequest.java
+++ src/main/java/com/taskflowapp/domain/auth/dto/request/AuthRegisterRequest.java
@@ -14,9 +14,14 @@ public class AuthRegisterRequest {
     @Pattern(regexp = "^[A-Za-z0-9]+$", message = "아이디는 영문과 숫자만 허용됩니다.")
     private String username;
 
+    // 기본 기능 요구사항에선 "유효한 이메일 형식"만 조건
+    // -> 엄격한 검증 조건 추가
     @NotBlank(message = "이메일은 필수 입력값입니다.")
     @Email(message = "이메일 형식이 올바르지 않습니다.")
-    @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$")
+    @Pattern(regexp = "^(?!\\.)[A-Za-z0-9._%+-]+(?<!\\.)@" +
+            "[A-Za-z0-9](?:[A-Za-z0-9-]*[A-Za-z0-9])?" +
+            "(?:\\.[A-Za-z0-9](?:[A-Za-z0-9-]*[A-Za-z0-9])?)*" +
+            "\\.[A-Za-z]{2,}$")
     private String email;
 
     // 기본 기능 요구사항에선 "최소 8자 이상"만 조건
@@ -25,7 +30,7 @@ public class AuthRegisterRequest {
     @Size(min = 8, max = 64)
     @Pattern(
             regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[!@#$%^&*(),.?\":{}|<>]).{8,64}$",
-            message = "비밀번호는 대소문자 불문 영문, 숫자, 특수문자를 모두 포함해야 합니다.")
+            message = "비밀번호는 대소문자 불문 영문, 숫자, 특수문자를 모두 포함해야 하며, 8~64자여야 합니다.")
     private String password;
 
     @Size(min = 2, max = 50, message = "이름은 2~50자여야 합니다.")
