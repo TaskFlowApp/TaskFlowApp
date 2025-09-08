a520e26e48f7ef2cb122e1433228dc8d6c8aa954
jinhee kim <lsa68803@naver.com>
2025-09-09 03:51:57 +0900

    [team_info] refactor: 반복응답 코드 정적 팩토리 메서드 적용

--- src/main/java/com/taskflowapp/domain/team/controller/TeamController.java
+++ src/main/java/com/taskflowapp/domain/team/controller/TeamController.java
@@ -20,6 +20,9 @@ import java.util.List;
 public class TeamController {
 
     private final TeamService teamService;
+    // try-catch 문
+    // 전역 예외 클래스 생성하게 되면, 성공 응답 외 삭제 예정
+    // 팀 없음 에러 -> UI상 예외 발생 X => 보안상 구현
 
     // 팀 생성 //
     @PostMapping("/teams")
@@ -27,19 +30,19 @@ public class TeamController {
             @Valid @RequestBody TeamRequest teamRequest,
             @AuthenticationPrincipal UserDetailsImpl userDetails
     ) {
-        // 전역 예외 클래스 생성 후 성공 응답 외 삭제 예정
+        // 성공 응답
         try {
             TeamResponse createdTeam = teamService.createTeam(teamRequest, userDetails);
 
             return ResponseEntity.status(HttpStatus.CREATED)
                     .body(ApiResponse.success("팀이 성공적으로 생성되었습니다.", createdTeam));
 
-        // 팀 중복 에러 처리
+        // 실패응답 (중복 팀 에러)
         } catch (IllegalArgumentException e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                     .body(ApiResponse.error(e.getMessage()));
 
-        // 관리자 아닐 경우 에러 처리
+        // 실패응답 (권한 에러)
         } catch (ResponseStatusException e) {
             return ResponseEntity.status(HttpStatus.FORBIDDEN)
                     .body(ApiResponse.error(e.getMessage()));
@@ -52,6 +55,7 @@ public class TeamController {
     ) {
         List<TeamResponse> teamResponseList = teamService.getAllTeams();
 
+        // 성공응답
         return ResponseEntity.status(HttpStatus.OK)
                 .body(ApiResponse.success("팀 목록을 조회했습니다.", teamResponseList));
     }
@@ -64,14 +68,14 @@ public class TeamController {
     public ResponseEntity<ApiResponse<TeamResponse>> findTeamById(
             @PathVariable Long teamId
     ) {
-        // 전역 예외 클래스 생성 후 성공 응답 외 삭제 예정
+        // 성공응답
         try {
             TeamResponse teamResponse = teamService.getTeam(teamId);
 
             return ResponseEntity.status(HttpStatus.OK)
                     .body(ApiResponse.success("팀 정보를 조회했습니다.",  teamResponse));
 
-        // 팀 없음 에러 처리 -> UI상 예외 발생 X => 보안상 구현
+        // 실패응답 (팀 없음 에러)
         } catch (IllegalArgumentException e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND)
                     .body(ApiResponse.error(e.getMessage()));
@@ -85,25 +89,23 @@ public class TeamController {
             @PathVariable Long teamId,
             @AuthenticationPrincipal UserDetailsImpl userDetails
     ) {
-        // 전역 예외 클래스 생성 후 성공 응답 외 삭제 예정
+        // 성공응답
         try {
             TeamResponse teamUpdateResponse = teamService.updateTeam(teamRequest, teamId, userDetails);
 
             return ResponseEntity.status(HttpStatus.OK)
                     .body(ApiResponse.success("팀 정보가 성공적으로 업데이트되었습니다.", teamUpdateResponse));
 
-        // 팀 없음, 팀 중복 에러 처리 -> UI상 예외 발생 X => 보안상 구현
+        // 실패응답 (팀 없음 ,팀 중복 에러)
         } catch (IllegalArgumentException e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND)
                     .body(ApiResponse.error(e.getMessage()));
 
-        // 관리자 아닐 경우 에러 처리
+        // 실패응답 (권한 에러)
         } catch (ResponseStatusException e) {
             return ResponseEntity.status(HttpStatus.FORBIDDEN)
                     .body(ApiResponse.error(e.getMessage()));
         }
-
-
     }
 
     // 팀 삭제 //
@@ -112,19 +114,19 @@ public class TeamController {
             @PathVariable Long teamId,
             @AuthenticationPrincipal UserDetailsImpl userDetails
     ) {
-        // 전역 예외 클래스 생성 후 성공 응답 외 삭제 예정
+        // 성공응답
         try {
             DeleteTeamResponse deleteTeamResponse = teamService.deleteTeam(teamId, userDetails);
 
             return ResponseEntity.status(HttpStatus.OK)
                     .body(ApiResponse.success("팀이 성공적으로 삭제되었습니다.", deleteTeamResponse));
 
-        // 팀 없음 에러 처리 -> UI상 예외 발생 X => 보안상 구현
+        // 실패응답 (팀 없음 에러)
         } catch (IllegalArgumentException e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND)
                     .body(ApiResponse.error(e.getMessage()));
 
-        // 관리자 아닐 경우 에러 처리
+        // 실패응답 (권한 에러)
         } catch (ResponseStatusException e) {
             return ResponseEntity.status(HttpStatus.FORBIDDEN)
                     .body(ApiResponse.error(e.getMessage()));
--- src/main/java/com/taskflowapp/domain/team/service/TeamService.java
+++ src/main/java/com/taskflowapp/domain/team/service/TeamService.java
@@ -6,7 +6,6 @@ import com.taskflowapp.domain.team.dto.TeamRequest;
 import com.taskflowapp.domain.team.dto.TeamResponse;
 import com.taskflowapp.domain.team.entity.Team;
 import com.taskflowapp.domain.team.repository.TeamRepository;
-import com.taskflowapp.domain.user.dto.response.MemberResponseDto;
 import com.taskflowapp.domain.user.enums.UserRole;
 import com.taskflowapp.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -14,7 +13,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
-import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
@@ -30,68 +28,48 @@ public class TeamService {
     @Transactional
     public TeamResponse createTeam(TeamRequest teamRequest, UserDetailsImpl userDetails) {
 
-        // 관리자만 팀 생성 가능
+        // 1. 권한 검증 (관리자만 팀 생성 가능)
         if (!Objects.equals(userDetails.getAuthUser().getRole(), UserRole.ADMIN)) {
             throw new ResponseStatusException(HttpStatus.FORBIDDEN, "팀 생성은 관리자만 가능합니다.");
         }
 
-        // 팀 중복 에러
+        // 2. 중복 검증 (중복 팀 에러)
         if (teamRepository.existsByName(teamRequest.getName())) {
             throw new IllegalArgumentException("팀 이름이 이미 존재합니다.");
         }
 
+        // 3. 엔티티 생성 (요청 DTO -> Team 엔티티)
         Team team = Team.builder()
                 .name(teamRequest.getName())
                 .description(teamRequest.getDescription())
                 .build();
 
+        // 4. 엔티티 저장 (DB 저장)
         Team savedTeam = teamRepository.save(team);
 
-        return TeamResponse.builder()
-                .id(savedTeam.getId())
-                .name(savedTeam.getName())
-                .description(savedTeam.getDescription())
-                .createdAt(savedTeam.getCreatedAt())
-                .members(Collections.emptyList())
-                // Collections.emptyList() : 변경 불가능한 빈 리스트 -> 명시적으로 '비어 있음' 보장
-                .build();
+        // 5. DTO 변환/반환
+        // -> 정적 팩토리 메서드 사용
+        return TeamResponse.from(savedTeam);
     }
+
 
     // 팀 전체 목록 조회 //
     @Transactional(readOnly = true)
     public List<TeamResponse> getAllTeams() {
 
-        // 1. 엔티티 저장 (DB 저장) //
+        // 1. 엔티티 조회 (DB 에서 팀 목록 불러오기)
         List<Team> teams = teamRepository.findAllTeams();
 
-        // 2. DTO 변환 (엔티티 리스트 -> DTO 리스트) //
+        // 2. DTO 변환 (엔티티 리스트 -> DTO 리스트)
+        // -> 정적 팩토리 메서드 사용
         List<TeamResponse> teamResponses = teams.stream()
-                .map(team -> TeamResponse.builder()
-                        .id(team.getId())
-                        .name(team.getName())
-                        .description(team.getDescription())
-                        .createdAt(team.getCreatedAt())
-                        .members(
-                                // Stream<User> 생성 //
-                                team.getMembers().stream()
-                                        // Stream<MemberResponseDto> 로 변환
-                                        .map( member -> MemberResponseDto.builder()
-                                                .id(member.getId())
-                                                .username(member.getUsername())
-                                                .name(member.getName())
-                                                .email(member.getEmail())
-                                                .role(member.getRole())
-                                                .createdAt(member.getCreatedAt())
-                                                .build()
-                                        )
-                                        // List<MemberResponseDto> 로 변환 //
-                                        .collect(Collectors.toList())
-                        )
-                        .build()
-                )
+                .map(team -> TeamResponse.from(team))
                 .collect(Collectors.toList());
+
+        // 3. 변환된 DTO 반환
         return teamResponses;
     }
+
 
     // 특정 팀 조회 //
     // 현재 프론트(팀관리 > 팀 탭 전환)에서 동작하는 게 아님
@@ -100,103 +78,77 @@ public class TeamService {
     @Transactional(readOnly = true)
     public TeamResponse getTeam(Long teamId) {
 
-        // 1. id 조회
+        // 1. Team 엔티티 조회 (id 조회)
         Team team = teamRepository.findById(teamId).orElseThrow(
                 () -> new IllegalArgumentException("팀을 찾을 수 없습니다.")
         );
 
-        // 2. dto 변환
-        return TeamResponse.builder()
-                .id(team.getId())
-                .name(team.getName())
-                .description(team.getDescription())
-                .createdAt(team.getCreatedAt())
-                .members(
-                        team.getMembers().stream()
-                                .map(member -> MemberResponseDto.builder()
-                                        .id(member.getId())
-                                        .username(member.getUsername())
-                                        .name(member.getName())
-                                        .email(member.getEmail())
-                                        .role(member.getRole())
-                                        .createdAt(member.getCreatedAt())
-                                        .build()
-                                )
-                                .collect(Collectors.toList())
-                )
-                .build();
+        // 2. DTO 변환(엔티티 -> DTO) / 반환
+        // -> 정적 팩토리 메서드 사용
+        return TeamResponse.from(team);
     }
+
 
     // 팀 수정 //
     @Transactional
     public TeamResponse updateTeam(TeamRequest teamRequest, Long teamId, UserDetailsImpl userDetails) {
 
-        // 관리자만 팀 수정 가능
+        // 1. 권한 검증 (관리자만 팀 생성 가능)
         if (!Objects.equals(userDetails.getAuthUser().getRole(), UserRole.ADMIN)) {
             throw new ResponseStatusException(HttpStatus.FORBIDDEN, "팀 수정은 관리자만 가능합니다.");
         }
 
-        // 팀 없음 에러
+        // 2. 팀 엔티티 조회 (팀 없음 에러)
         Team team = teamRepository.findById(teamId).orElseThrow(
                 () -> new IllegalArgumentException("팀을 찾을 수 없습니다.")
         );
 
-        // 팀 중복 에러
+        // 3. 중복 검증 (중복 팀 에러)
         if (teamRepository.existsByName(teamRequest.getName())) {
             throw new IllegalArgumentException("팀 이름이 이미 존재합니다.");
         }
 
+        // 4. 엔티티 변경 (수정)
         team.changeTeam(
                 teamRequest.getName(),
                 teamRequest.getDescription()
         );
 
+        // 5. 엔티티 저장
         teamRepository.save(team);
 
-        return TeamResponse.builder()
-                .id(team.getId())
-                .name(team.getName())
-                .description(team.getDescription())
-                .createdAt(team.getCreatedAt())
-                .members(
-                        team.getMembers().stream()
-                                .map(member -> MemberResponseDto.builder()
-                                        .id(member.getId())
-                                        .username(member.getUsername())
-                                        .name(member.getName())
-                                        .email(member.getEmail())
-                                        .role(member.getRole())
-                                        .createdAt(member.getCreatedAt())
-                                        .build())
-                                .collect(Collectors.toList()))
-                .build();
+        // 6. DTO 변환(엔티티 -> DTO) / 반환
+        // -> 정적 팩토리 메서드 사용
+        return TeamResponse.from(team);
     }
+
 
     // 팀 삭제 //
     @Transactional
     public DeleteTeamResponse deleteTeam(Long teamId, UserDetailsImpl userDetails) {
 
-        // 관리자만 팀 삭제 가능
+        // 1. 권한 검증 (관리자만 팀 생성 가능)
         if (!Objects.equals(userDetails.getAuthUser().getRole(), UserRole.ADMIN)) {
             throw new ResponseStatusException(HttpStatus.FORBIDDEN, "팀 삭제는 관리자만 가능합니다.");
         }
 
-        // 팀 없음 에러
+        // 2. 팀 엔티티 조회 (팀 없음 에러)
         Team team = teamRepository.findById(teamId).orElseThrow(
                 () -> new IllegalArgumentException("팀을 찾을 수 없습니다.")
         );
 
-        // 팀에 속한 멤버들 팀 해제
+        // 3. 팀에 속한 멤버들 팀 해제
         // 해당 팀 소속된 모든 유저의 team 필드 null로 수정
         // null로 수정 안하면, 팀에 멤버 추가 시 삭제된 팀의 유저가 안뜸
         team.getMembers().forEach(
                 user -> user.changeTeam(null)
          );
 
-        // 팀 삭제 -> 소프트 딜리트로 변경
-        // DB 에서 삭제 대신 deleted = true
+        // 4. 팀 삭제 (소프트 딜리트)
+        // DB 에서 삭제 대신 'deleted = true' 처리
         team.softDelete();
 
+        // 5. DTO 반환 (삭제 완료 메세지)
         return new DeleteTeamResponse("팀이 성공적으로 삭제되었습니다");
     }
 }
